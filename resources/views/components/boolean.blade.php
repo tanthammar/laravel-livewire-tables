9617f6cd49dd568b1486b330b68989256817d27f
tanthammar <tinahammar@gmail.com>
2020-04-18 19:26:23 +0200

    smaller icons new array syntax

--- resources/views/components/boolean.blade.php
+++ resources/views/components/boolean.blade.php
@@ -3,18 +3,17 @@
     @endforeach
     >
     @php
-        $value = $options['jsonKeyVal'] ? ($model->{$options['column']}[$options['key']] || false)
-        :($model->{$options['column']} || false);
-        $true_class = $options['icon']['true-class'] ? $options['icon']['true-class'] : 'text-green-500';
-        $false_class = $options['icon']['false-class'] ? $options['icon']['false-class'] : 'text-red-500';
-        $true = $options['icon']['true'] ? $options['icon']['true'] : 'solid/toggle-on';
-        $false = $options['icon']['false'] ? $options['icon']['false'] : 'solid/toggle-off';
-        $text = $options['text'] ?? '';
+        $value = \Arr::has($options, 'jsonKeyVal') ? ($model->{$options['column']}[$options['key']] || false) : ($model->{$options['column']} || false);
+        $true_class = \Arr::get($options, 'icon.true-class', 'text-green-500');
+        $false_class = \Arr::get($options, 'icon.false-class', 'text-red-500');
+        $true = \Arr::get($options, 'icon.true', 'solid/toggle-on');
+        $false = \Arr::get($options, 'icon.false', 'solid/toggle-off');
+        $text = \Arr::get($options, 'text', '');
     @endphp
     @if($value)
-        @svg($true, "h-8 w-8 mx-2 inline-flex {$true_class}")
+        @svg($true, "h-6 w-6 mx-2 inline-flex {$true_class}")
     @else
-        @svg($false, "h-8 w-8 mx-2 inline-flex {$false_class}")
+        @svg($false, "h-6 w-6 mx-2 inline-flex {$false_class}")
     @endif
     {{ $text }}
 </span>
