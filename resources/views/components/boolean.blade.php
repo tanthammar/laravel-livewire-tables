4f588039b15bb136d9d68be2e85d37320f551248
tanthammar <tinahammar@gmail.com>
2020-04-18 02:26:27 +0200

    moved text

--- resources/views/components/boolean.blade.php
+++ resources/views/components/boolean.blade.php
@@ -3,23 +3,18 @@
     @endforeach
     >
     @php
-        $value = $options['jsonKeyVal'] ? ($model->{$options['column']}[$options['key']] || false) :($model->{$options['column']} || false);
+        $value = $options['jsonKeyVal'] ? ($model->{$options['column']}[$options['key']] || false)
+        :($model->{$options['column']} || false);
         $true_class = $options['icon']['true-class'] ? $options['icon']['true-class'] : 'text-green-500';
         $false_class = $options['icon']['false-class'] ? $options['icon']['false-class'] : 'text-red-500';
         $true = $options['icon']['true'] ? $options['icon']['true'] : 'solid/toggle-on';
         $false = $options['icon']['false'] ? $options['icon']['false'] : 'solid/toggle-off';
         $text = $options['text'] ?? '';
-        if($value) {
-            echo $true;
-        } else {
-            echo $false;
-        }
-        echo $text;
     @endphp
     @if($value)
         @svg($true, "h-8 w-8 mx-2 inline-flex {$true_class}")
     @else
         @svg($false, "h-8 w-8 mx-2 inline-flex {$false_class}")
-    {{ $text }}
     @endif
+    {{ $text }}
 </span>
